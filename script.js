f863f7fcc258c845315d9623774fedb82b824bef
Yash-Chaudhary-Official <yashchaudharychaudhary48@gmail.com>
2023-09-02 15:02:40 +0530

    add welcome , better comments

---
+++ script.js
@@ -0,0 +1,36 @@
+//alert("hii")
+
+setTimeout(function () {
+    document.querySelector(".welcomee").classList.add("welcome");
+},2000)
+var a=document.querySelectorAll(".like-btn")
+var b=document.querySelectorAll(".likes")
+//0-n-1
+for(let i=0;i<a.length;i++){
+    a[i].addEventListener("click",function(){
+        var  count=b[i].textContent;
+       // alert(count.length)
+        count=count.substring(0,count.length-3)
+        count=parseInt(count)
+        
+       b[i].textContent=(count+1)+" likes";
+
+       // alert(count+1)
+        
+    });
+}
+var cinput=document.querySelectorAll(".input")
+var dcomment=document.querySelectorAll(".lim")
+var ebutton=document.querySelectorAll(".post-comment-btn ")
+//alert(dcomment.length)
+for(let i=0;i<ebutton.length;i++){
+    ebutton[i].addEventListener("click",function(){
+       var f=cinput[i].value;
+       //alert(f)
+       cinput[i].value="";
+       f=" "+f;
+       dcomment[i].innerHTML="<strong> Comment:</strong>"+f;   
+
+        
+    });
+}
